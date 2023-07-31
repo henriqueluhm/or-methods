b4a9e72cb2a62109d2ca22f99b2a856dc427078b
Henrique Luhm <62779084+henriqueluhm@users.noreply.github.com>
2023-07-31 02:20:48 -0300

    feat: added footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,24 @@
-import { ReactElement } from "react";
+import { ReactElement } from 'react';
+import IconAnchor from '../IconAnchor/IconAnchor';
+
+import githubSvg from '../../../public/github.svg';
+import linkedInSvg from '../../../public/linkedIn.svg';
 
 export default function Footer(): ReactElement {
-    return <footer className="w-full h-[40px] flex items-center justify-center font-thin text-sm">developed by:</footer>
+	return (
+		<footer className='w-full h-[40px] flex-center font-thin text-sm xl:text-base gap-x-2'>
+			developed by:{' '}
+			<IconAnchor
+				alt='github'
+				href='https://github.com/henriqueluhm'
+				src={githubSvg}
+				rounded
+			/>
+			<IconAnchor
+				alt='linkedIn'
+				href='https://www.linkedin.com/in/henrique-luhm-a319a71b0/'
+				src={linkedInSvg}
+			/>
+		</footer>
+	);
 }
---
+++ src/components/IconAnchor/types.ts
@@ -0,0 +1,8 @@
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
+
+export interface IconAnchorProperties {
+  alt: string;
+  href: string;
+  src: StaticImport;
+  rounded?: boolean;
+}
